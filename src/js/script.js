621df5685973d7374681ed0747f675a1bc48d4b7
BNaFTH <chananontheparak@gmail.com>
2024-10-30 16:23:17 +0700

    Initial Commit

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+/*scroll section active link*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+
+
+window.onscroll = () => {
+	sections.forEach(sec => {
+		let top = window.scrollY
+		let offset = sec.offsetTop - 150
+		let height = sec.offsetHeight;
+		let id = sec.getAttribute('id');
+
+		if(top >= offset && top < offset + height) {
+			navLinks.forEach(links => {
+				links.classList.remove('active');
+				document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+			});
+		};
+
+	});
+
+
+
+/* sticky navbar */
+let header = document.querySelector('.header');
+/*let logo = document.querySelector('.logo');*/
+
+header.classList.toggle('sticky', window.scrollY > 100);
+/*logo.classList.toggle('sticky',window.scrollY > 100);*/
+};
